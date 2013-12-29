20887e0b9eac58d2252a5b8a4608c79460de4b75
Simone Renzo <simone201@gmail.com>
2013-12-29 17:02:44 +0100

    ramdisk: updated and optimised with selinux support

--- ramdisk/sbin/fkbootscript.sh
+++ ramdisk/sbin/fkbootscript.sh
@@ -3,12 +3,13 @@
 # disable sysctl.conf to prevent ROM interference with tunables
 # backup and replace PowerHAL with custom build to allow OC/UC to survive screen off
 # create and set permissions for /system/etc/init.d if it doesn't already exist
-mount -o rw,remount /system;
+mount -o rw,remount /system /system;
 [ -e /system/etc/sysctl.conf ] && mv /system/etc/sysctl.conf /system/etc/sysctl.conf.fkbak;
 [ -f /system/lib/hw/power.msm8974.so.bak ] || mv /system/lib/hw/power.msm8974.so /system/lib/hw/power.msm8974.so.bak
+[ -f /system/bin/thermal-engine-hh-bak ] || mv /system/bin/thermal-engine-hh /system/bin/thermal-engine-hh-bak
 if [ ! -e /system/etc/init.d ]; then
   mkdir /system/etc/init.d
   chown -R root.root /system/etc/init.d;
   chmod -R 755 /system/etc/init.d;
 fi;
-mount -o ro,remount /system;
+mount -o ro,remount /system /system;
