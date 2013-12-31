e669447727a3f003c9e548f84cdc916b2201b7aa
Simone Renzo <simone201@gmail.com>
2013-12-31 13:36:07 +0100

    cpufreq_interactive: fixing merge errors

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -128,8 +128,8 @@ static bool io_is_busy = false;
  * touched. input_boost needs to be enabled.
  */
 #define DEFAULT_INPUT_BOOST_FREQ 1497600
-static int input_boost_freq = DEFAULT_INPUT_BOOST_FREQ;
-extern u64 last_input_time;
+int input_boost_freq = DEFAULT_INPUT_BOOST_FREQ;
+extern unsigned long last_input_time;
 
 #define CPU_SYNC_FREQ 1036800
 
